f359403baa00612d544bc9bbb411deec65cff369
AvSharma31 <135153271+AvSharma31@users.noreply.github.com>
2024-02-04 10:08:40 +0530

    Update Answer_2_Python.py

--- Answer_2_Python.py
+++ Answer_2_Python.py
@@ -9,6 +9,10 @@ def highest_frequency_word_length(input_string):
 
     return len(highest_frequency_word)
 
-input_string = "Hello my name is Avani Sharma"
-result = highest_frequency_word_length(input_string)
-print(f"Length of the highest-frequency word: {result}")
+input_string_1 = "write write write all the number from from from 1 to 100"
+result_1 = highest_frequency_word_length(input_string_1)
+print(f"Length of the highest-frequency word: {result_1}")
+
+input_string_2 = "Hello world! Hello Python world! world! world!"
+result_2 = highest_frequency_word_length(input_string_2)
+print(f"Length of the highest-frequency word: {result_2}")
